f036404ffdebb70e94c5b12483cc9357cd4416ad
Georgedata23 <0727gm@gmail.com>
2025-01-13 15:04:33 +0300

    2

--- img_text/mainimg/urls.py
+++ img_text/mainimg/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('upload_doc/<int:id_doc>/', views.upload),
+    path('upload_doc/<slug:id_doc>/', views.upload_slug),
+
 ]
--- img_text/mainimg/views.py
+++ img_text/mainimg/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Страница приложения mainimg!")
+
+def upload(request, id_doc):
+    return HttpResponse(f"<h2>Страница загрузки файла</h2><p>id: {id_doc}</p>")
+
+def upload_slug(request, id_doc):
+    return HttpResponse(status=422, content="Uncorrected id, use integer!")

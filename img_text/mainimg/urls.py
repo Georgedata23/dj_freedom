f6d3d865baa3fa9b488b97eb61815b3b56e75928
Georgedata23 <0727gm@gmail.com>
2025-01-14 17:04:58 +0300

    5

--- img_text/mainimg/urls.py
+++ img_text/mainimg/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('upload_doc/<slug:id_doc_slug>/', views.upload_slug, name='pydantic is cool!'),
     path('analyse/<int:id_doc>/', views.analyse, name='analyse'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('protected/', views.ProtectedPageView.as_view(), name='protected'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login'), name='logout'),
 
 ]

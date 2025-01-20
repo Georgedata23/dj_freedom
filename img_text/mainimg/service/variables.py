5d007edccae9e102d3b3789ddef6f61c0ff37547
Georgedata23 <0727gm@gmail.com>
2025-01-20 21:34:14 +0300

    10

--- img_text/mainimg/service/variables.py
+++ img_text/mainimg/service/variables.py
@@ -4,7 +4,7 @@ from random import randint
 random_id = randint(1, 10000)
 menu_index = {'analyse': "Проанализировать картинку",
               'upload': "Добавить картинку", 'id': random_id,
-              'get_text': "Получить текст"}
+              'get_text': "Получить текст", 'home': "Главная страница"}
 
 
 data_df = [

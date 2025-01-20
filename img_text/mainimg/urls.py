06a873e5969a6af1c61c0143f12003be70f9b323
Georgedata23 <0727gm@gmail.com>
2025-01-20 18:14:39 +0300

    9

--- img_text/mainimg/urls.py
+++ img_text/mainimg/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('upload_doc/<slug:id_doc_slug>/', views.upload_slug, name='pydantic is cool!'),
     path('analyse/', views.analyse, name='analyse'),
     path('login/', auth_views.LoginView.as_view(template_name='mainimg/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/login'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login/'), name='logout'),
     path('delete/', views.DeleteFormView.as_view(), name='delete')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

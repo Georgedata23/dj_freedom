2d0a0f03185768f64b90d97889f3eb432920f2e4
Georgedata23 <0727gm@gmail.com>
2025-01-20 17:24:25 +0300

    9

--- img_text/mainimg/apps.py
+++ img_text/mainimg/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class MainimgConfig(AppConfig):
+    verbose_name = "Изображние в текст"
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mainimg'
